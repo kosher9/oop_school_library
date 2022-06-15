5b668e1106593b30f02f5dee68322d2a6373f4cd
Cédric Kossi <lionel.kossi@gmail.com>
2022-06-15 22:28:12 +0000

    Create relation beteween person-rental-book

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 require './nameable'
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rentals
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -9,6 +9,11 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals << rental
   end
 
   def can_use_service?
--- rental.rb
+++ rental.rb
@@ -1,7 +1,11 @@
 class Rental
-  attr_accessor :date
+  attr_accessor :date, :student, :book
 
-  def initialize(date)
+  def initialize(date, person, book)
     @date = date
+    @person = person
+    person.add_rental(self)
+    @book = book
+    book.add_rental(self)
   end
 end
